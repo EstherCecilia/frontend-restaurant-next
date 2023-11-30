59a80a5e5f96d8fd5931f78fd7178ae16a5ff1cf
Esther Oliveira <esther.oliveira@olxbr.com>
2023-11-30 09:17:11 -0300

    feat: add tags

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 
 interface ButtonProps {
+  id: string;
   menu_type: string;
   url: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ menu_type, url }) => {
+const Button: React.FC<ButtonProps> = ({ id, menu_type, url }) => {
   return (
-    <button className="s:w-[50%] 3xl:w-[50%] block w-[60%] p-[1%] justify-content align-content bg-caribbean-blue border-solid rounded-button-border-radius border-button-orange border-2 text-xl uppercase m-button-margin">
+    <button
+      id={id}
+      className="s:w-[50%] 3xl:w-[50%] block w-[60%] p-[1%] justify-content align-content bg-caribbean-blue border-solid rounded-button-border-radius border-button-orange border-2 text-xl uppercase m-button-margin"
+    >
       <a
         className="hover:text-medium-brown visited:text-medium-brown"
         href={url}
--- src/containers/MenuButtons/index.tsx
+++ src/containers/MenuButtons/index.tsx
@@ -4,12 +4,18 @@ import Button from "../../components/Button";
 const MenuButtons = () => {
   return (
     <section className="pt-12">
-      <Button url="/blackgyalbites-nextjs/menu-pdf-food.pdf" menu_type="Food" />
       <Button
+        id="button-food"
+        url="/blackgyalbites-nextjs/menu-pdf-food.pdf"
+        menu_type="Food"
+      />
+      <Button
+        id="button-drinks"
         url="/blackgyalbites-nextjs/menu-pdf-drinks.pdf"
         menu_type="Drinks"
       />
       <Button
+        id="button-catering"
         url="/blackgyalbites-nextjs/menu-pdf-catering.pdf"
         menu_type="Catering"
       />
